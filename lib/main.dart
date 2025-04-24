2ee286218f66e3703462068e5f56a009c899e1b0
Ramzan <mhdramzan.dev@gmail.com>
2025-04-24 12:40:03 +0500

    gemini api added -- sending request

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:colorist_ui/colorist_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'providers/gemini.dart';
+import 'services/gemini_chat_service.dart';
+
 void main() async {
   runApp(ProviderScope(child: MainApp()));
 }
@@ -11,26 +14,22 @@ class MainApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final model = ref.watch(geminiModelProvider);
+
     return MaterialApp(
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: MainScreen(
-        sendMessage: (message) {
-          sendMessage(message, ref);
-        },
+      home: model.when(
+        data:
+            (data) => MainScreen(
+          sendMessage: (text) {
+            ref.read(geminiChatServiceProvider).sendMessage(text);
+          },
+        ),
+        loading: () => LoadingScreen(message: 'Initializing Gemini Model'),
+        error: (err, st) => ErrorScreen(error: err),
       ),
     );
-  }
-
-  void sendMessage(String message, WidgetRef ref) {
-    debugPrint("sendMessage->$message");
-    final chatStateNotifier = ref.read(chatStateNotifierProvider.notifier);
-    final logStateNotifier = ref.read(logStateNotifierProvider.notifier);
-
-    chatStateNotifier.addUserMessage(message);
-    logStateNotifier.logUserText(message);
-    chatStateNotifier.addLlmMessage(message, MessageState.complete);
-    logStateNotifier.logLlmText(message);
   }
 }
